8e9699202e1552d542e6d608937b1bbead291718
webdev0320 <muhammadarbaz894@gmail.com>
2026-02-11 23:12:49 +0500

    Update EmpWallet.jsx

--- src/pages/EmpWallet.jsx
+++ src/pages/EmpWallet.jsx
@@ -237,7 +237,7 @@ const EmpWallet = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100">
+  <div className="p-6">
       <h1 className="text-xl font-bold mb-4 px-4">Wallet</h1>
 
       {/* ================= BALANCE CARD ================= */}
