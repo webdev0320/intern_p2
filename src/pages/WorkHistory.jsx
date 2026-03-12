0df27764f17cab7a4302e4adcc98dd0a148e7b5c
webdev0320 <muhammadarbaz894@gmail.com>
2026-03-12 17:24:26 +0500

    Update WorkHistory.jsx

--- src/pages/WorkHistory.jsx
+++ src/pages/WorkHistory.jsx
@@ -313,7 +313,7 @@ const handleStartWork = async (job, worker) => {
     if (response.ok) {
       Swal.fire("Started!", "Job is now in progress.", "success");
     } else {
-      Swal.fire("Failed!", data.message, "error");
+      Swal.fire("Ops!", data.message, "warning");
     }
 
   } catch (error) {
